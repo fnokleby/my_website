a77106ddaf658820476ebb211c277871c087f429
Filip Nøkleby <48674942+fnokleby@users.noreply.github.com>
2021-09-03 12:36:12 +0200

    Initiall commit

---
+++ index.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var app = express();
+app.set('view engine', 'pug');
+
+const si = require('systeminformation');
+const fs = require('fs');
+
+var cpuTemp;
+
+app.get('/', function (req, res) {
+
+    si.cpuTemperature(function(data) {
+        cpuTemp = data;
+
+        console.log(cpuTemp)
+      });
+
+    res.render('index', { title: 'Hey', cpu_temp: cpuTemp.main});
+    console.log('Someone connected!');
+    // si.cpuTemperature().then(data => { return data})
+    
+});
+
+app.get('/cpu', function (req, res) {
+    res.send(cpuTemp)
+    console.log(cpuTemp)
+});
+
+app.listen(80, function () {
+    console.log('Listening to Port 80');
+});
